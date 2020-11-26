8a9b31bc0cb601fa19de928254dd333e6cba5319
Natalie-Cardot <nataliecardot@gmail.com>
2020-11-26 13:25:20 -0800

    Output users

--- script.js
+++ script.js
@@ -32,4 +32,38 @@ async function getRandomUser() {
 // Add new user object to data array
 function addData(obj) {
   data.push(obj);
+
+  updateDOM();
 }
+
+// Update DOM
+// Setting default value of data array (ES6 syntax)
+function updateDOM(providedData = data) {
+  // Clear main div
+  main.innerHTML = '<h2><strong>Person</strong> Wealth</h2>';
+
+  // Old style of looping, before forEach
+  // for (i = 0, i < providedData.length, i++) {
+  //   providedData[i]
+  // }
+
+  // forEach is from ES5
+  providedData.forEach((item) => {
+    const element = document.createElement('div');
+    element.classList.add('person');
+    element.innerHTML = `<strong>${item.name}</strong> ${formatMoney(
+      item.money
+    )}`;
+    // Insert newly created element into DOM
+    main.appendChild(element);
+  });
+}
+
+// Format money value
+function formatMoney(number) {
+  // Regular expression matches a single digit \d followed by a three-digit sets only (?=(\d{3})+(?!\d)). The matched digit is then replaced with $1,. The $1 is a special replacement pattern which holds a value of the first parenthesized sub-match string (in our case it is the matched digit)
+  return '$' + number.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,');
+}
+
+// Event listeners
+addUserBtn.addEventListener('click', getRandomUser);
